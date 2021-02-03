503d1d7c5676b6fe49402ab42fd378e51d0096f2
tobtoht <thotbot@protonmail.com>
2021-02-03 02:34:03 +0100

    Don't link RandomX

--- src/cryptonote_core/cryptonote_tx_utils.h
+++ src/cryptonote_core/cryptonote_tx_utils.h
@@ -144,11 +144,13 @@ namespace cryptonote
     );
 
   class Blockchain;
+
+#ifndef BUILD_GUI_DEPS
   bool get_block_longhash(const Blockchain *pb, const blobdata& bd, crypto::hash& res, const uint64_t height, const int major_version, const crypto::hash *seed_hash, const int miners = 0);
   bool get_block_longhash(const Blockchain *pb, const block& b, crypto::hash& res, const uint64_t height, const crypto::hash *seed_hash = nullptr, const int miners = 0);
   crypto::hash get_block_longhash(const Blockchain *pb, const block& b, const uint64_t height, const crypto::hash *seed_hash = nullptr, const int miners = 0);
   void get_altblock_longhash(const block& b, crypto::hash& res, const crypto::hash& seed_hash);
-
+#endif
 }
 
 BOOST_CLASS_VERSION(cryptonote::tx_source_entry, 1)
