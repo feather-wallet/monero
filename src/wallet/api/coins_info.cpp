78c5d04c7a17161d3eff81850d32adcb397bc17b
tobtoht <thotbot@protonmail.com>
2021-07-01 23:09:28 +0200

    Coins: description

--- src/wallet/api/coins_info.cpp
+++ src/wallet/api/coins_info.cpp
@@ -113,6 +113,10 @@ string CoinsInfoImpl::pubKey() const {
 bool CoinsInfoImpl::coinbase() const {
     return m_coinbase;
 }
+
+string CoinsInfoImpl::description() const {
+    return m_description;
+}
 } // namespace
 
 namespace Bitmonero = Monero;
