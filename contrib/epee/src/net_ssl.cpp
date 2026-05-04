c6acb57ac7d0f8e1e75642505cd49771a1ae0699
ryurt46 <robin.yurt1@gmail.com>
2026-05-04 19:01:04 +0200

    net: replace deprecated boost::asio::strand::wrap with bind_executor

--- contrib/epee/src/net_ssl.cpp
+++ contrib/epee/src/net_ssl.cpp
@@ -36,6 +36,7 @@
 #include <boost/asio/strand.hpp>
 #include <condition_variable>
 #include <boost/lambda/lambda.hpp>
+#include <boost/asio/bind_executor.hpp>
 #include <openssl/ssl.h>
 #include <openssl/pem.h>
 #include "misc_log_ex.h"
@@ -614,7 +615,7 @@ bool ssl_options_t::handshake(
         socket.async_handshake(
           type,
           boost::asio::buffer(buffer),
-          strand.wrap(on_handshake)
+          boost::asio::bind_executor(strand, on_handshake)
         );
       }
     );
